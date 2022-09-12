1f5033de68c9b96c53efc9c6b77c6c098d5fb30c
Julian Arias <julian.arias@datavail.com>
2022-09-12 17:07:20 -0500

    initial commit

---
+++ AzureAdLogsClient.Core/LogsClient.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading.Tasks;
+using Azure.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Graph;
+
+namespace AzureAdLogsClient.Core
+{
+    public interface ILogsClient
+    {
+        Task<IAuditLogRootSignInsCollectionPage?> GetAuditLogs();
+    }
+
+    public class LogsClient: ILogsClient
+    {
+        private GraphServiceClient _graphClient;
+        
+        public LogsClient(IConfiguration configuration)
+        {
+            // application should have this api permissions in portal "AuditLog.Read.All", "Directory.Read.All"
+            var scopes = new[] {"https://graph.microsoft.com/.default"};
+            
+            var tenantId =  configuration["TenantId"];
+            var clientId = configuration["ClientId"];
+            var clientSecret = configuration["ClientSecret"];
+            
+            var options = new TokenCredentialOptions
+            {
+                AuthorityHost = AzureAuthorityHosts.AzurePublicCloud
+            };
+            
+            var deviceCodeCredential = new ClientSecretCredential(tenantId, clientId, clientSecret, options);
+
+            _graphClient = new GraphServiceClient(deviceCodeCredential, scopes);
+        }
+
+        public async Task<IAuditLogRootSignInsCollectionPage?> GetAuditLogs()
+        {
+            var lastDay = new Date(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day);
+            var auditLogs = await _graphClient.AuditLogs.SignIns
+                .Request()
+                .Filter($"createdDateTime gt {lastDay}") // filter date
+                .GetAsync();
+            return auditLogs;
+        }
+    }
+}
---
+++ AzureAdLogsClient/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using AzureAdLogsClient.Core;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace AzureAdLogsClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var host = CreateHostBuilder(args).Build();
+
+            var logsClient = host.Services.GetService<ILogsClient>();
+
+            var data = await logsClient.GetAuditLogs();
+
+            Console.ReadLine();
+        }
+        
+        private static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            var hostBuilder = Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((context, builder) =>
+                {
+                    builder.SetBasePath(Directory.GetCurrentDirectory());
+                })
+                .ConfigureServices((context, services) =>
+                {
+                    services.AddTransient<ILogsClient, LogsClient>();
+                });
+
+            return hostBuilder;
+        }
+    }
+}
